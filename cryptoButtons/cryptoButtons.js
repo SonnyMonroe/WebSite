7ada0dcd22f2515c770581e685e3517cf9012f85
Michael <mabramo11@yahoo.com>
2023-02-10 11:13:56 -0500

    added buttonsa

--- cryptoButtons/cryptoButtons.js
+++ cryptoButtons/cryptoButtons.js
@@ -10,6 +10,12 @@ const avaxBtn = document.getElementById("avax-btn");
 const dogeBtn = document.getElementById("doge-btn");
 const shibaBtn = document.getElementById("shiba-btn");
 const solanaBtn = document.getElementById("solana-btn");
+const adaBtn = document.getElementById("ada-btn");
+const okbBtn = document.getElementById("okb-btn");
+const lidoBtn = document.getElementById("lido-btn");
+const ltcBtn = document.getElementById("ltc-btn");
+const dotBtn = document.getElementById("dot-btn");
+const tronBtn = document.getElementById("tron-btn");
 
 const result = document.querySelector("#result");
 
@@ -110,6 +116,54 @@ const solanaResponses = [
   "Solana is built with a focus on decentralization and has a strong commitment to open-source development, allowing anyone to contribute to the growth and improvement of the platform.",
 ];
 
+const adaResponses = [
+  "Cardano is also designed to be highly customizable, allowing developers to build and deploy their own decentralized applications on the platform.",
+  "Cardano is written in the Haskell programming language, making it one of the most secure blockchain platforms.",
+  "The platform uses a unique proof-of-stake consensus algorithm called Ouroboros, which is more energy-efficient than other algorithms.",
+  "Cardano has a governance model that allows token holders to vote on proposals for upgrades and changes to the platform.",
+  "The Creator of Cardano Charles Hoskinson is a strong advocate for the use of blockchain technology in solving real-world problems and has been a speaker at various conferences and events around the world. He is also active on social media and regularly shares his insights on the future of blockchain and cryptocurrency.",
+];
+
+const okbResponses = [
+  "OKX is an innovative cryptocurrency exchange with advanced financial services.",
+  "OKB is used as a utility token on the OKEx platform, providing users with access to various benefits and discounts.",
+  "This Centralized Exchange serves millions of users in over 100 countries.",
+  "OKB provides spot, margin, futures, options, perpetual swaps trading, DeFi, lending, and mining services.",
+  "OKB has partnerships with several companies and organizations in the blockchain and cryptocurrency space, including the blockchain platform, Chainlink, and the Ethereum-based gaming platform, Enjin.",
+];
+
+const lidoResponses = [
+  "Lido Staked Ether (Lido ETH) is a token that represents a stake in the Lido Ethereum 2.0 validator.",
+  "Lido ETH is used to participate in the validation of transactions on the Ethereum 2.0 network, which is a major upgrade to the Ethereum network.",
+  "Lido ETH is designed to make it easier for individuals to participate in the Ethereum 2.0 network and earn rewards, without having to set up and run their own validator.",
+  "Lido ETH is fully decentralized and permissionless, allowing anyone to participate and earn rewards.",
+  "Lido ETH also provides governance rights to its holders, allowing them to vote on proposals for upgrades and changes to the Ethereum 2.0 network.",
+];
+
+const dotResponses = [
+  "Polkadot is designed to be scalable, secure, and flexible, allowing for the creation of multi-chain applications and decentralized finance (DeFi) platforms.",
+  "Polkadot (DOT) is the native token of the Polkadot network and is used to participate in the network's governance and to pay for transaction fees.",
+  "The platform uses a unique proof-of-stake consensus algorithm called Ouroboros, which is more energy-efficient than other algorithms.",
+  "Gavin Wood is a co-founder of Polkadot and the Web3 Foundation, and was also the former Chief Technology Officer of Ethereum.",
+  "DOT is a innovative cryptocurrency that operates on a unique consensus mechanism, allowing for efficient and secure validation of transactions across different blockchain networks.",
+];
+
+const ltcResponses = [
+  "Litecoin is based on the same open-source code as Bitcoin and is often referred to as the 'silver to Bitcoin's gold'.",
+  "Litecoin is a cryptocurrency that was created in 2011 by Charlie Lee, a former Google engineer.",
+  "Litecoin can be used for a variety of purposes, including peer-to-peer payments, micropayments, and as a store of value.",
+  "Litecoin is widely accepted by merchants and exchanges and has a strong ecosystem of wallets, exchanges, and payment processors.",
+  "Litecoin has a rich history and has been one of the longest-standing cryptocurrencies in the market, with a proven track record of stability and security.",
+];
+
+const tronResponses = [
+  "Tron Founder Justin Sun is known for his forward-thinking and ambitious vision for the future of blockchain and cryptocurrency, and has been a strong advocate for the mass adoption of decentralized technologies.",
+  "Tron CEO Justin Sun is a well-known figure in the cryptocurrency and blockchain space, known for his marketing skills and strong presence on social media.",
+  "Justin Sun is a young entrepreneur and the founder of TRON, as well as the CEO of BitTorrent and CEO of Callme.",
+  "TRON's native cryptocurrency is called TRX, which can be used for various purposes, including paying for content on the TRON network and participating in decentralized applications.",
+  "TRON aims to create a decentralized internet by enabling content creators to freely publish, store and own their data without the interference of intermediaries.",
+];
+
 bitcoinBtn.addEventListener("click", () => {
   result.textContent =
     bitcoinResponses[Math.floor(Math.random() * bitcoinResponses.length)];
@@ -168,6 +222,36 @@ shibaBtn.addEventListener("click", () => {
 solanaBtn.addEventListener("click", () => {
   result.textContent =
     solanaResponses[Math.floor(Math.random() * solanaResponses.length)];
+});
+
+adaBtn.addEventListener("click", () => {
+  result.textContent =
+    adaResponses[Math.floor(Math.random() * adaResponses.length)];
+});
+
+okbBtn.addEventListener("click", () => {
+  result.textContent =
+    okbResponses[Math.floor(Math.random() * okbResponses.length)];
+});
+
+lidoBtn.addEventListener("click", () => {
+  result.textContent =
+    lidoResponses[Math.floor(Math.random() * lidoResponses.length)];
+});
+
+ltcBtn.addEventListener("click", () => {
+  result.textContent =
+    ltcResponses[Math.floor(Math.random() * ltcResponses.length)];
+});
+
+dotBtn.addEventListener("click", () => {
+  result.textContent =
+    dotResponses[Math.floor(Math.random() * dotResponses.length)];
+});
+
+tronBtn.addEventListener("click", () => {
+  result.textContent =
+    tronResponses[Math.floor(Math.random() * tronResponses.length)];
 });
 
 const showResponse = (responses) => {
